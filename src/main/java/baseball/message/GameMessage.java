ff6cd5a26a77a9f494a13f61227323825a18cf33
BOYOUNG-KANG <28287@naver.com>
2023-09-11 21:21:08 +0900

    feat(InputView): 플레이어 숫자 입력받기 추가

--- src/main/java/baseball/message/GameMessage.java
+++ src/main/java/baseball/message/GameMessage.java
@@ -2,4 +2,5 @@ package baseball.message;
 
 public class GameMessage {
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,2 +1,13 @@
-package baseball.view;public class InputView {
+package baseball.view;
+
+import baseball.message.GameMessage;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class InputView {
+    public String getPlayerNumber(){
+        System.out.println(GameMessage.INPUT_NUMBER_MESSAGE);
+        return Console.readLine();
+    }
 }
