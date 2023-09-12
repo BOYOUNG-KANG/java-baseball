a73effe77629dab1aeba5231a939f13491332c40
BOYOUNG-KANG <28287@naver.com>
2023-09-12 12:38:57 +0900

    feat(GameRestartChecker): 재시작 입력값 예외 처리 추가

--- src/main/java/baseball/constant/BaseballConstant.java
+++ src/main/java/baseball/constant/BaseballConstant.java
@@ -8,4 +8,6 @@ public class BaseballConstant {
     public static final String STRIKE = "스트라이크 ";
     public static final String BALL = "볼 ";
     public static final String NOTHING = "낫싱";
+    public static final int RESTART = 1;
+    public static final int END = 2;
 }
--- src/main/java/baseball/domain/ScoreCalculator.java
+++ src/main/java/baseball/domain/ScoreCalculator.java
@@ -50,5 +50,4 @@ public class ScoreCalculator {
         }
         return strike;
     }
-
 }
