2590ac29d9f00b811bc66f1530db2e3102328c61
BOYOUNG-KANG <28287@naver.com>
2023-09-12 10:57:17 +0900

    feat(BaseballConstant): 게임 결과 상수 추가

--- src/main/java/baseball/constant/BaseballConstant.java
+++ src/main/java/baseball/constant/BaseballConstant.java
@@ -5,4 +5,7 @@ public class BaseballConstant {
     public static final int MAX_NUMBER = 9;
     public static final int NUMBER_SIZE = 3;
     public static final String VALIDATE_NUMBER_ZERO = "0";
+    public static final String STRIKE = "스트라이크 ";
+    public static final String BALL = "볼 ";
+    public static final String NOTHING = "낫싱";
 }
