5ebe2bb135540aa1f2ad07e4e34ae59f537ba9d0
BOYOUNG-KANG <28287@naver.com>
2023-09-12 11:17:28 +0900

    feat(BaseballController): 게임 결과 출력 로직 추가

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -2,6 +2,7 @@ package baseball.controller;
 
 import baseball.domain.BaseballRandomNumberGenerator;
 import baseball.domain.PlayerNumber;
+import baseball.domain.ScoreCalculator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
@@ -21,5 +22,58 @@ public class BaseballController {
         List<Integer> randomNumbers = setRandomNumber();
         PlayerNumber playerNumber = new PlayerNumber();
         playerNumber.updatePlayerNumbers(inputView.getPlayerNumber());
+        List<Integer> playerNumbers = playerNumber.getPlayerNumbers();
+
+        ScoreCalculator calculator = new ScoreCalculator();
+        calculator.calculateNumbers(randomNumbers, playerNumbers);
+
+        printScore(calculator);
+    }
+    private void printScore(ScoreCalculator calculator){
+
+        int strike = calculator.getStrike();
+        int ball = calculator.getBall();
+        int nothing = calculator.getNothing();
+
+        printStrike3(strike);
+        printNothing(nothing);
+        printOnlyStrike(strike, ball);
+        printOnlyBall(strike, ball);
+        printStrikeAndBall(strike, ball);
+    }
+
+    private void printStrikeAndBall(int strike, int ball) {
+        if (0 < strike && strike < 3 && 0 < ball){
+            outputView.printScoreBall(ball);
+            outputView.printScoreStrike(strike);
+            System.out.println();
+        }
+    }
+
+    private void printOnlyBall(int strike, int ball) {
+        if (0 < ball && strike == 0) {
+            outputView.printScoreBall(ball);
+            System.out.println();
+        }
+    }
+
+    private void printOnlyStrike(int strike, int ball) {
+        if (0 < strike && strike < 3 && ball == 0) {
+            outputView.printScoreStrike(strike);
+            System.out.println();
+        }
+    }
+
+    private void printNothing(int nothing) {
+        if (nothing == 1) {
+            outputView.printScoreNothing();
+        }
+    }
+
+    private void printStrike3(int strike) {
+        if (strike == 3) {
+            outputView.printScoreStrike(strike);
+            outputView.printGameEnd();
+        }
     }
 }
