6cb36157030acbcfd2189f8d7ca2a4db8fc5380e
BOYOUNG-KANG <28287@naver.com>
2023-09-12 11:16:46 +0900

    feat(OutputView): 게임 결과 및 종료 메시지 출력 로직 생성

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,10 +1,23 @@
 package baseball.view;
 
+import baseball.constant.BaseballConstant;
 import baseball.message.GameMessage;
 
 public class OutputView {
     public void printGameStart(){
         System.out.println(GameMessage.GAME_START_MESSAGE);
     }
-
+    public void printScoreStrike(int strike){
+        System.out.print(strike + BaseballConstant.STRIKE);
+    }
+    public void printScoreBall(int ball){
+        System.out.print(ball + BaseballConstant.BALL);
+    }
+    public void printScoreNothing(){
+        System.out.println(BaseballConstant.NOTHING);
+    }
+    public void printGameEnd(){
+        System.out.println();
+        System.out.println(GameMessage.GAME_END_MESSAGE);
+    }
 }
