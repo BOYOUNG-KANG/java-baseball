da09720d74c4d26286e6b26ef67ff478169ddaa3
BOYOUNG-KANG <28287@naver.com>
2023-09-17 00:58:37 +0900

    refactor: LinkedHashSet 사용해 중복 확인 함수 삭제

--- src/main/java/baseball/domain/BaseballRandomNumberGenerator.java
+++ src/main/java/baseball/domain/BaseballRandomNumberGenerator.java
@@ -5,22 +5,21 @@ import baseball.constant.BaseballConstant;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class BaseballRandomNumberGenerator {
 
     public List<Integer> generateRandomNumber(){
-        List<Integer> randomNumbers = new ArrayList<>();
+        LinkedHashSet<Integer> randomNumbers = new LinkedHashSet<>();
         while (randomNumbers.size() < BaseballConstant.NUMBER_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(BaseballConstant.MIN_NUMBER, BaseballConstant.MAX_NUMBER);
-            checkNumberDuplicates(randomNumbers, randomNumber);
-        }
-        return randomNumbers;
-    }
-
-    private static void checkNumberDuplicates(List<Integer> randomNumbers, int randomNumber) {
-        if (!randomNumbers.contains(randomNumber)) {
             randomNumbers.add(randomNumber);
         }
+        return convertToList(randomNumbers);
+    }
+
+    private static List<Integer> convertToList(LinkedHashSet<Integer> randomNumbers) {
+        return new ArrayList<>(randomNumbers);
     }
 }
